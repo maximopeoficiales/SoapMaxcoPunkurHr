bc7ac949e9a6841b074f364111dbef0c792e9663
Maximo Junior Apaza Chirhuana <maximopeoficiales@gmail.com>
2020-10-09 02:47:58 -0500

    dominio dinamico

--- init.php
+++ init.php
@@ -1,10 +1,13 @@
 <?php
+define('WP_USE_THEMES', false);
+require('../wp-blog-header.php');
 require "vendor/autoload.php";
 require __DIR__ . "/WebServices.php";
-$wsdlGenerator =  new \PHP2WSDL\PHPClass2WSDL("WebServices", "http://maxco.punkuhr.test/webservices/initialize.php");
+$dominio = get_site_url();
+$wsdlGenerator =  new \PHP2WSDL\PHPClass2WSDL("WebServices", "$dominio/webservices/init.php");
 $wsdlGenerator->generateWSDL();
 $wsdlGenerator->save(__DIR__ . "/example.wsdl");
 $soap = new SoapServer(__DIR__ . "/example.wsdl");
 $soap->setClass('WebServices');
 $soap->handle();
-// return ["msg" => "wsdl generado y funcionando correctamente."];
+header("Location: $dominio/wp-json/webservice/wsdl");
