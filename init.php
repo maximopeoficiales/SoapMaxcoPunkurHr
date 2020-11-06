9ade95252a473c022b8933c4ba6a0b070b69fdce
Maximo Junior Apaza Chirhuana <maximopeoficiales@gmail.com>
2020-11-06 05:02:09 -0500

    obtencion de url

--- init.php
+++ init.php
@@ -3,19 +3,30 @@
 error_reporting(0);
 
 require_once(__DIR__ . '/WebServices.php');
-// require_once(__DIR__ . '/wsdl-creator/class.phpwsdl.php');
-
-// $soap = PhpWsdl::RunQuickMode(
-//      array(                                        // All files with WSDL definitions in comments
-//           'WebServices.php',
-//      )
-// );
+function url_completa($forwarded_host = false)
+{
+     $ssl   = !empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on';
+     $proto = strtolower($_SERVER['SERVER_PROTOCOL']);
+     $proto = substr($proto, 0, strpos($proto, '/')) . ($ssl ? 's' : '');
+     if ($forwarded_host && isset($_SERVER['HTTP_X_FORWARDED_HOST'])) {
+          $host = $_SERVER['HTTP_X_FORWARDED_HOST'];
+     } else {
+          if (isset($_SERVER['HTTP_HOST'])) {
+               $host = $_SERVER['HTTP_HOST'];
+          } else {
+               $port = $_SERVER['SERVER_PORT'];
+               $port = ((!$ssl && $port == '80') || ($ssl && $port == '443')) ? '' : ':' . $port;
+               $host = $_SERVER['SERVER_NAME'] . $port;
+          }
+     }
+     return $proto . '://' . $host;
+}
 
 $options = [
      'soap_version' => SOAP_1_1,
      'cache_wsdl' => WSDL_CACHE_NONE
 ];
-
-$server = new SoapServer('http://maxco.punkuhr.test/webservices/wsdl.php', $options);
+$urld=url_completa();
+$server = new SoapServer("$urld/webservices/wsdl.php", $options);
 $server->setClass('WebServices');
 $server->handle();
