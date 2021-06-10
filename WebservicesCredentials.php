e34026922168fc11f18047724835725a9cac6be3
maximopeoficiales <maximopeoficiales@gmail.com>
2021-06-10 02:58:29 -0500

    se agregaron credenciales

--- WebservicesCredentials.php
+++ WebservicesCredentials.php
@@ -3,22 +3,22 @@ class WebservicesCredentials
 {
     public $HOST_DB = "localhost";
 
-    public $PRECOR_URL = "http://tiendaqaprecor.test/";
-    public $MAXCO_URL = "http://tiendaqamaxco.test/";
+    public $PRECOR_URL = "https://tiendaqa.precor.pe/";
+    public $MAXCO_URL = "https://maxco.punkuhr.com/";
 
-    public $DB_PRECOR_USER = "root";
-    public $DB_PRECOR_PASS = "";
-    public $DB_PRECOR_DBNAME = "tiendaqa_precor";
+    public $DB_PRECOR_USER = "clg_wp_3oxdh";
+    public $DB_PRECOR_PASS = "Iz3r_0!Pe4faK2d&";
+    public $DB_PRECOR_DBNAME = "clg_wp_retpq";
 
-    public $DB_MAXCO_USER = "root";
-    public $DB_MAXCO_PASS = "";
-    public $DB_MAXCO_DBNAME = "tiendaqamaxco";
+    public $DB_MAXCO_USER = "i5142852_wp4";
+    public $DB_MAXCO_PASS = "F.L7tJxfhTbrfbpP7Oe41";
+    public $DB_MAXCO_DBNAME = "i5142852_wp4";
 
     // woocommerce 
 
     public $WOO_PRECOR_CK = "ck_82458af7253f4bbd4bd0941f5487323f31b23cdf";
     public $WOO_PRECOR_CS = "cs_b0aa70d51f7757485699c97342568b85e9922af2";
 
-    public $WOO_MAXCO_CK = "ck_d4af9c8fa9e3d5ecdf536c72150080869aeef021";
-    public $WOO_MAXCO_CS = "cs_31832af01184ba14da85c608e44c2bfe53d0214c";
+    public $WOO_MAXCO_CK = "ck_0157c4f5fbc72b4a71161b929dea276a81006fd9";
+    public $WOO_MAXCO_CS = "cs_b575ce513cbaf2478ca0d06c2d0dd64699ec642d";
 }
