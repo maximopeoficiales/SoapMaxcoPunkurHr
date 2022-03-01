c78429dd38126d566c117fd085b239aa733128b3
Maximo Junior Apaza Chirhuana <maximopeoficiales@gmail.com>
2022-03-01 09:30:13 -0500

    ADD: inicio de integracion

---
+++ WebservicesCredentials copy.php
@@ -0,0 +1,33 @@
+<?php
+class WebservicesCredentials
+{
+    public $HOST_DB = "localhost";
+
+    public $PRECOR_URL = "https://tiendaqa.precor.pe/";
+    public $MAXCO_URL = "https://tiendaqa.maxco.pe/";
+
+    public $DB_PRECOR_USER = "clg_wp_3oxdh";
+    public $DB_PRECOR_PASS = "Iz3r_0!Pe4faK2d&";
+    public $DB_PRECOR_DBNAME = "clg_wp_retpq";
+
+    public $DB_MAXCO_USER = "clg_wp_kae5v";
+    public $DB_MAXCO_PASS = "TT68X5$5ckV@D*WD";
+    public $DB_MAXCO_DBNAME = "clg_wp_gak7t";
+
+    // woocommerce 
+
+    public $WOO_PRECOR_CK = "ck_82458af7253f4bbd4bd0941f5487323f31b23cdf";
+    public $WOO_PRECOR_CS = "cs_b0aa70d51f7757485699c97342568b85e9922af2";
+
+    public $WOO_MAXCO_CK = "ck_ed40a83456e5f389286708093899ab45384078b7";
+    public $WOO_MAXCO_CS = "cs_dcb486eb5c60fa052188bdb6f8eb98b4c3e92739";
+
+    // IZIPAY
+    public $IZI_PAY_USERNAME = "12158862";
+    public $IZI_PAY_PASSWORD = "testpassword_o7ct19x6LOUuMNuRsLT8AlzNHbu88p4jHWy7hhsSPRypn";
+
+    function getHeaderBasicIziPay(): string
+    {
+        return "Basic " . base64_encode($this->IZI_PAY_USERNAME . ':' . $this->IZI_PAY_PASSWORD);
+    }
+}
